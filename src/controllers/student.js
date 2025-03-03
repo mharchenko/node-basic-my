dd769728f30e440b7f0fe01e59c9a1e0694be701
Mexty <m.harchenko@i.ua>
2025-03-03 20:39:50 +0200

    HTTP

--- src/controllers/student.js
+++ src/controllers/student.js
@@ -1,6 +1,12 @@
 // import { getAllStudents, getStudentById } from '../services/students.js';
 
-import { getAllStudents, getStudentById } from '../services/student.js';
+import {
+  createStudent,
+  deleteStudent,
+  getAllStudents,
+  getStudentById,
+  updateStudent,
+} from '../services/student.js';
 import createHttpError from 'http-errors';
 // export const getStudentsController = async (req, res) => {
 //   const students = await getAllStudents();
@@ -58,5 +64,64 @@ export const getStudentByIdController = async (req, res, next) => {
     status: 200,
     message: `Successfully found student with id ${studentId}!`,
     data: student,
+  });
+};
+
+export const createStudentController = async (req, res) => {
+  const student = await createStudent(req.body);
+
+  res.status(201).json({
+    status: 201,
+    message: `Successfully created a student!`,
+    data: student,
+  });
+};
+
+export const deleteStudentController = async (req, res, next) => {
+  const { studentId } = req.params;
+
+  const student = await deleteStudent(studentId);
+
+  if (!student) {
+    next(createHttpError(404, 'Student not found'));
+    return;
+  }
+
+  res.status(204).send();
+};
+
+export const upsertStudentController = async (req, res, next) => {
+  const { studentId } = req.params;
+
+  const result = await updateStudent(studentId, req.body, {
+    upsert: true,
+  });
+
+  if (!result) {
+    next(createHttpError(404, 'Student not found'));
+    return;
+  }
+
+  const status = result.isNew ? 201 : 200;
+
+  res.status(status).json({
+    status,
+    message: `Successfully upserted a student!`,
+    data: result.student,
+  });
+};
+export const patchStudentController = async (req, res, next) => {
+  const { studentId } = req.params;
+  const result = await updateStudent(studentId, req.body);
+
+  if (!result) {
+    next(createHttpError(404, 'Student not found'));
+    return;
+  }
+
+  res.json({
+    status: 200,
+    message: `Successfully patched a student!`,
+    data: result.student,
   });
 };
--- src/routers/student.js
+++ src/routers/student.js
@@ -5,6 +5,10 @@ import { Router } from 'express';
 import {
   getStudentsController,
   getStudentByIdController,
+  createStudentController,
+  deleteStudentController,
+  upsertStudentController,
+  patchStudentController,
 } from '../controllers/student.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
@@ -15,5 +19,9 @@ const router = Router();
 router.get('/students', ctrlWrapper(getStudentsController));
 
 router.get('/students/:studentId', ctrlWrapper(getStudentByIdController));
+router.post('/students', ctrlWrapper(createStudentController));
+router.delete('/students/:studentId', ctrlWrapper(deleteStudentController));
+router.put('/students/:studentId', ctrlWrapper(upsertStudentController));
+router.patch('/students/:studentId', ctrlWrapper(patchStudentController));
 
 export default router;
